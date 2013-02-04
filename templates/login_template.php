ee70d7f64ddd6fcbec553f06dc4222499eaf4395
theflowersshesaidshesent <theflowersshesaidshesent@b7c36bab-8956-b2a8-0f3b-98a6af9334da>
2013-02-04 21:14:03 +0000

    Fixed warning on creation page when logged out Added "returnto" value for login.php Working on merging creation pages--not done yet

--- templates/login_template.php
+++ templates/login_template.php
@@ -10,7 +10,7 @@
 <? require_once("header.php"); ?>
 <div class="container">
 <h1>Login</h1>
-<div>Enter your username and password to log in. <a href="?action=register">But I don't have an account!</a></div><br/>
+<div>Enter your username and password to log in. <a href="?action=register&returnto=<?php echo $_GET['returnto']; ?>">But I don't have an account!</a></div><br/>
 <form method="post">
 <label style="margin-right:5px;" for="user">Username:</label><input type="text" name="user" /><br/>
 <label style="margin-right:8px;" for="pass">Password:</label><input type="password" name="pass" /><br/>
