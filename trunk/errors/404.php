4165d76097eb7d3f3bc46c6a091a239ba7d3c867
theflowersshesaidshesent <theflowersshesaidshesent@b7c36bab-8956-b2a8-0f3b-98a6af9334da>
2013-02-15 21:16:24 +0000

    Lots of formatting changes

--- trunk/errors/404.php
+++ trunk/errors/404.php
@@ -1,6 +1,8 @@
-<?php header("HTTP/1.0 404 Not Found"); ?>
+<?php
+header("HTTP/1.0 404 Not Found");
+?>
 <!DOCTYPE html>
-<?
+<?php
 //Include config
 $split_path=explode("\\",dirname(__FILE__));
 for($k=0;$k<count($split_path);$k++){
@@ -21,20 +23,28 @@ error_reporting(E_ALL ^ E_NOTICE);
 session_start();
 ?>
 <html>
-<head>
-<title>404 | <? echo SITE_NAME ?></title>
-<link rel="stylesheet" type="text/css" href="<? echo BASE_URL?>/templates/style.php" media="screen" />
-
-</head>
-
-<body>
-<? require_once(BASE_DIRECTORY."/templates/header.php") ?>
-<div class="container">
-<h1>404 error</h1>
-<img src="<? echo BASE_URL ?>/errors/404.png"/><br/>
-Whatever's supposed to be here isn't. Oops.<br/><br/>
-<a href="<? echo BASE_URL ?>/">Back to home</a>
-</div>
-</body>
+	<head>
+		<title>
+			404 | <?php echo SITE_NAME ?>
+			
+		</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>/templates/style.php" media="screen" />
+	</head>
+	<body>
+		<?php require_once(BASE_DIRECTORY."/templates/header.php") ?>
+		<div class="container">
+			<h1>404 error</h1>
+			<div class="errorimage">
+				<img src="<?php echo BASE_URL ?>/errors/404.png"/>
+			</div>
+			<div class="errordescription">
+				Whatever's supposed to be here isn't. Oops.
+				<br/><br/>
+				<a href="<?php echo BASE_URL ?>/">Back to home</a>
+			</div>
+		</div>
+	</body>
 </html>
-<? die ?>
+<?php
+die();
+?>
