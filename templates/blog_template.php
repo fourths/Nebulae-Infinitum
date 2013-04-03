c7d289b4bfd746be50b0b9efbafdc06679a1b5c5
theflowersshesaidshesent <theflowersshesaidshesent@b7c36bab-8956-b2a8-0f3b-98a6af9334da>
2013-04-03 20:42:29 +0000

    Added getting data from database and display of blog posts on both user & the admin blogs

--- templates/blog_template.php
+++ templates/blog_template.php
@@ -28,9 +28,26 @@
 				?>
 			</div>
 			<div class="blog_left" style="float:left;width:210px;height:600px;background-color:white;">
-			
+				<h2 style="margin:0px;margin-left:10px;margin-top:10px;">Posts</h2>
 			</div>
 			<div class="blog_right" style="float:right;width:580px;height:600px;background-color:white;">
+			<?php
+			foreach($posts as $post){
+				echo '<div class="blog_post" style="margin:10px;padding:10px;background-color:gainsboro;" id="post'.$post['postid'].'">
+					<h2 style="margin:0px;">'.$post['title'].'</h2>';
+					
+				// Inform the user of who the post is by if it's on the admin blog
+				if($type=="admin"){
+					echo '<div class="postedby" style="color:grey;">Posted by <a style="color:grey" href="../user.php?id='.$post['userid'].'">'.get_username_from_id($post['userid']).'</a> at '.date("g:ia m/d/y T",strtotime($post['timestamp'])).'</div>';
+				}
+				else{
+					echo '<div class="postedby" style="color:grey;">Posted at '.date("g:ia m/d/y T",strtotime($post['timestamp'])).'</div>';
+				}
+				echo '<div class="blog_post_content">'.bbcode_parse($post['content']).'</div>';
+				
+				echo '</div>';
+			}
+			?>
 			
 			</div>
 			<div style="clear:both;"></div>
