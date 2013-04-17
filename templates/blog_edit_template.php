daa56cd52e0a3f864bbcad83427a8dd2f9eb6c07
theflowersshesaidshesent <theflowersshesaidshesent@b7c36bab-8956-b2a8-0f3b-98a6af9334da>
2013-04-17 20:44:11 +0000

    Starting blog posting

--- templates/blog_edit_template.php
+++ templates/blog_edit_template.php
@@ -1,51 +1,13 @@
 <!DOCTYPE html>
-<?php 
-//Auto-select the current license on the drop-down
-$selected='selected="selected"';
-switch($creation['license']){
-	case 'copyright':
-		$a=$selected;
-		break;
-	case 'cc-0':
-		$b=$selected;
-		break;
-	case 'cc-by':
-		$c=$selected;
-		break;
-	case 'cc-by-nd':
-		$d=$selected;
-		break;
-	case 'cc-by-sa':
-		$e=$selected;
-		break;
-	case 'cc-by-nc':
-		$f=$selected;
-		break;
-	case 'cc-by-nc-nd':
-		$g=$selected;
-		break;
-	case 'cc-by-nc-sa':
-		$h=$selected;
-		break;
-	case 'mit':
-		$i=$selected;
-		break;
-	case 'gpl':
-		$j=$selected;
-		break;
-	case 'bsd':
-		$k=$selected;	
-		break;
-}
-
-if ($creation['hidden'] == "no" || $creation['hidden'] == "approved") $nselected = 'selected="selected"';
-else if ($creation['hidden'] == "byowner" || $creation['hidden'] == "deleted") $hselected = 'selected="selected"';
-else if ($creation['hidden'] == "censored" || $creation['hidden'] == "flagged") $cselected = 'selected="selected"';
-?>
 <html>
 	<head>
-		<title>
-			Edit blog post | <?php echo SITE_NAME; ?>
+		<title><?php
+				if ($action=="edit"){
+					echo "Edit blog post";
+				}
+				else{
+					echo "New blog post";
+				}?> | <?php echo SITE_NAME; ?>
 			
 		</title>
 		<link rel="stylesheet" type="text/css" href="../templates/style.php" media="screen" />
@@ -55,20 +17,28 @@ else if ($creation['hidden'] == "censored" || $creation['hidden'] == "flagged") 
 		<?php require_once("header.php"); ?>
 		<div class="container">
 			<div class="editheader">
-				<h2 style="display:inline;">Edit blog post</h2>
+				<h2 style="display:inline;"><?php
+					if ($action=="edit"){
+						echo "Edit blog post";
+					}
+					else{
+						echo "New blog post";
+					}?></h2>
 			</div>
 			<div>
-				<input type="text" name="title" style="margin-left:0px;width:350px;" placeholder="Title" value="<?php echo stripslashes($blog_post['title'])?>"/>
+				<input type="text" name="title" style="margin-left:0px;width:350px;" placeholder="Title" value="<?php echo stripslashes($blog_post['title']); ?>"/>
 			</div>
 			
 			<textarea name="post_body" style="width:795px;height:300px;max-height:600px;resize:vertical;font-family:Arial,Helvetica,sans-serif;display:block;" selected="selected"><?php
 				echo stripslashes($blog_post['content']);
 			?></textarea>
-			
-			<input type="submit" id="update" name="update" value="Update" style="margin-left:0px;"/>
 			<?php
-				if ($action="edit"){
-					echo '<input type="submit" id="update" name="update" value="Update" style="margin-left:0px;"/>';
+				if ($action=="edit"){
+					echo '<input type="submit" id="update" name="update" value="Update" style="margin-left:0px;"/>
+					<input type="submit" id="delete" name="delete" value="Delete" style="margin-left:0px;"/>';
+				}
+				else {
+					echo '<input type="submit" id="submit" name="submit" value="Submit" style="margin-left:0px;"/>';
 				}
 			?>
 			
