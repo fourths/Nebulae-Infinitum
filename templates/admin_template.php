7dbf7b58ea533636170c2c68bb6d6a37cd6474b5
theflowersshesaidshesent <theflowersshesaidshesent@b7c36bab-8956-b2a8-0f3b-98a6af9334da>
2013-05-20 20:17:09 +0000

    Cleaning / commenting dispatcher.php admin.php admin_template.php adminflags_template.php

--- templates/admin_template.php
+++ templates/admin_template.php
@@ -1,77 +1,129 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<title>Administration | <?php echo SITE_NAME ?></title>
-	<link rel="stylesheet" type="text/css" href="include/style.css" media="screen" />
+		<title>
+			Administration | <?php echo SITE_NAME; ?>
+			
+		</title>
+		<link rel="stylesheet" type="text/css" href="include/style.css" media="screen" />
 	</head>
 
 	<body>
-	<?php require_once("header.php"); ?>
-	<div class="container">
-		<h1>Administration</h1>
-		<div class="adminblock">
-		<h2 style="margin:0px;">Recent flags</h2>
-		<div class="flagblock">
-		<table>
-		<?php
-		$display_chars=100;
-		if (isset($flags)&&(int) $flags->fetch_array()!=0){
-		$flags->data_seek(0);
-			while($flag=$flags->fetch_array()){
-				unset($appendix);
-				if($flag['type']=="creation"){
-					$creationname=$mysqli->query("SELECT name FROM creations WHERE id=".$flag['parentid'])->fetch_array();
-					if (strlen($creationname[0])>$display_chars) $appendix = "&hellip;";
-					$creationname[0]=$creationname[0]==""?"<span style='color:#E00'>Deleted creation</span>":'<a class="td" href="creation/'.$flag['parentid'].'">'.trim(substr(strip_bbcode($creationname[0]),0,$display_chars)).$appendix.'</a>';
+	<?php
+	require_once( "header.php" );
+	?>
+		<div class="container">
+			<h1>Administration</h1>
+			<div class="adminblock">
+				<h2 style="margin:0px;">Recent flags</h2>
+				<div class="flagblock">
+					<table>
+						<?php
+						// Set the amount of characters for it to display in each box (and it will cut off w/ ellipses if it's longer)
+						$display_chars = 100;
+						
+						if ( isset( $flags ) && ( (int) $flags->fetch_array() ) != 0) {
+							$flags->data_seek( 0 );
+							while ( $flag = $flags->fetch_array() ) {
+								unset( $addendum );
+								switch ( $flag['type'] ) {
+									// Determine the type of the flag
+									case "creation":
+										$item_name = $mysqli->query( "SELECT name FROM creations WHERE id=".$flag['parentid'] )->fetch_array();
+										
+										// If the flag name is too long, use an ellipses to cut it off
+										if ( strlen( $item_name[0] ) > $display_chars ) {
+											$addendum = "&hellip;";
+										}
+										
+										// If the item name isn't set, just assume that the item is deleted
+										if ( $item_name[0] == "") {
+											$item_name[0] = '<span style="color:#E00">Deleted creation</span>';
+										}
+										else {
+											$item_name[0] = '<a class="td" href="creation/' . $flag['parentid'] . '">' . trim( substr( strip_bbcode( $item_name[0] ), 0, $display_chars ) ) . $addendum . '</a>';
+										}
+									break;
+									
+									case "comment":
+										$item_name = $mysqli->query( "SELECT comment FROM comments WHERE id=" . $flag['parentid'] )->fetch_array();
+										
+										if ( strlen( $item_name[0] ) > $display_chars ) {
+											$addendum = "&hellip;";
+										}
+										
+										if ( $item_name[0] == "") {
+											$item_name[0] = '<span style="color:#E00">Deleted comment</span>';
+										}
+										else {
+											$item_name[0] = '<a class="td" href="creation/' . get_creation_from_comment( $flag['parentid'], $mysqli ) . '#' . $flag['parentid'] . '">' . trim( substr( strip_bbcode( $item_name[0] ), 0, $display_chars ) ) . $addendum . '</a>';
+										}
+									break;
+									
+									case "message":
+										$item_name = $mysqli->query( "SELECT message FROM messages WHERE id=" . $flag['parentid'] )->fetch_array();
+										
+										if ( strlen( $item_name[0] ) > $display_chars ) {
+											$addendum = "&hellip;";
+										}
+										
+										if ( $item_name[0] == "") {
+											$item_name[0] = '<span style="color:#E00">Deleted message</span>';
+										}
+										else {
+											$item_name[0] = '<a class="td" href="messages/' . get_sender_from_message( $flag['parentid'], $mysqli ) . '#' . $flag['parentid'] . '">' . trim( substr( strip_bbcode( $item_name[0]), 0, $display_chars ) ) . $addendum . '</a>';
+										}
+									break;
+								}
+								echo '<tr id="' . $flag['id']. '">
+									<td class="' . $flag['type'] . '" style="width:200px;">' . $item_name[0] . '</td>
+									<td class="' . $flag['type'] . '" style="width:80px;"><a class="td" href="user/' . get_username_from_id( $flag['userid'], $mysqli ) . '">' . get_username_from_id( $flag['userid'], $mysqli ) . '</a></td>
+									<td class="' . $flag['type'] . '" style="width:80px;">' . date( "m/d/Y", strtotime( $flag['timestamp'] ) ) . '</td>
+									<td class="' . $flag['type'] . '" style="width:400px;">' . $flag['content'] . '<a class="deletebutton" href="admin/flags/' . $flag['id'] . '/delete"></a></td>
+								</tr>';
+							}
+						}
+						?>
+					</table>
+				</div>
+				<?php
+				// Display a link for the big flags page if there are more flags than will display here
+				if ( $flags_amount > ADMIN_FLAGS ) {
+					echo '<a href="?mode=flags" class="td">Show more &gt;</a>';
 				}
-				else if ($flag['type']=="comment"){
-					$creationname=$mysqli->query("SELECT comment FROM comments WHERE id=".$flag['parentid'])->fetch_array();
-					if (strlen($creationname[0])>$display_chars) $appendix = "&hellip;";
-					$creationname[0]=$creationname[0]==""?"<span style='color:#E00'>Deleted comment</span>":'<a class="td" href="creation/'.get_creation_from_comment($flag['parentid'], $mysqli).'#'.$flag['parentid'].'">'.trim(substr(strip_bbcode($creationname[0]),0,$display_chars)).$appendix.'</a>';
-				}
-				else if ($flag['type']=="message"){
-					$creationname=$mysqli->query("SELECT message FROM messages WHERE id=".$flag['parentid'])->fetch_array();
-					if (strlen($creationname[0])>$display_chars) $appendix = "&hellip;";
-					$creationname[0]=$creationname[0]==""?"<span style='color:#E00'>Deleted message</span>":'<a class="td" href="messages/'.get_sender_from_message($flag['parentid'],$mysqli).'#'.$flag['parentid'].'">'.trim(substr(strip_bbcode($creationname[0]),0,$display_chars)).$appendix.'</a>';
-				}
-				echo '<tr id="'.$flag['id'].'">
-				<td class="'.$flag['type'].'" style="width:200px;">'.$creationname[0].'</td>
-				<td class="'.$flag['type'].'" style="width:80px;"><a class="td" href="user/'.get_username_from_id($flag['userid'],$mysqli).'">'.get_username_from_id($flag['userid'],$mysqli).'</a></td>
-				<td class="'.$flag['type'].'" style="width:80px;">'.date("m/d/Y",strtotime($flag['timestamp'])).'</td>
-				<td class="'.$flag['type'].'" style="width:400px;">'.$flag['content'].'<a class="deletebutton" href="admin/flags/'.$flag['id'].'/delete"></a></td>
-				</tr>';
-			}
-		}
-		?>
-		</table>
+				?>
+			</div>
+			<div class="adminblock">
+				<h2 style="margin:0px;">User preferences</h2>
+				Enter a username and press submit to go to their preferences page.
+				<form method="post">
+					<input type="text" name="prefsusername" placeholder="Username"/>
+					<input type="submit" name="prefssubmit" value="Submit"/>
+				</form>
+			</div>
+			<div class="adminblock">
+				<h2 style="margin:0px;">User message history</h2>
+				Enter a username and press submit to view their entire saved message history.
+				<form method="post">
+					<input type="text" name="usernamemsg" placeholder="Username"/>
+					<input type="submit" name="msgsubmit" value="Submit"/>
+				</form>
+			</div>
+			<div class="adminblock">
+				<h2 style="margin:0px;">Admin messages</h2>
+				Use this form to send administrator messages.
+				<form method="post">
+					<input type="text" name="recipientusername" placeholder="Recipient username"/><br/>
+					
+					<textarea name="adminmessage" placeholder="Message" style="height:100px;width:200px;max-width:500px;max-height:300px;margin-left:2px;"></textarea><br/>
+					
+					<input type="checkbox" name="showuser" value="1"/>
+					Don't show sender username<br/>
+					
+					<input type="submit" name="adminmessagesubmit" value="Submit"/>
+				</form>
+			</div>
 		</div>
-		<?php if ($flagsamt>10) echo '<a href="?mode=flags" class="td">Show more &gt;</a>';?>
-		</div>
-		<div class="adminblock">
-		<h2 style="margin:0px;">User preferences</h2>
-		Enter a username and press submit to go to their preferences page.
-		<form method="post">
-		<input type="text" name="prefsusername" placeholder="Username"/><input type="submit" name="prefssubmit" value="Submit"/>
-		</form>
-		</div>
-		<div class="adminblock">
-		<h2 style="margin:0px;">User message history</h2>
-		Enter a username and press submit to view their entire saved message history.
-		<form method="post">
-		<input type="text" name="usernamemsg" placeholder="Username"/><input type="submit" name="msgsubmit" value="Submit"/>
-		</form>
-		</div>
-		<div class="adminblock">
-		<h2 style="margin:0px;">Admin messages</h2>
-		Use this form to send administrator messages.
-		<form method="post">
-		<input type="text" name="recipientusername" placeholder="Recipient username"/><br/>
-		<textarea name="adminmessage" placeholder="Message" style="height:100px;width:200px;max-width:500px;max-height:300px;margin-left:2px;"></textarea>
-		<br/>
-		<input type="checkbox" name="showuser" value="1">Don't show sender username
-		<br/>
-		<input type="submit" name="adminmessagesubmit" value="Submit"/>
-		</form>
 		</div>
 	</body>
 </html>
--- templates/adminflags_template.php
+++ templates/adminflags_template.php
@@ -2,14 +2,14 @@
 <html>
 	<head>
 		<title>
-			Administration / Flags | <?php echo SITE_NAME ?>
+			Administration / Flags | <?php echo SITE_NAME; ?>
 		
 		</title>
 		<link rel="stylesheet" type="text/css" href="../include/style.css" media="screen" />
 	</head>
 	<body>
 		<?php
-		require_once("header.php"); 
+		require_once( "header.php" ); 
 		?>
 		<div class="container">
 			<h1>Recent flags</h1>
@@ -17,26 +17,58 @@
 				<div class="flagblock">
 					<table>
 					<?php
-					if (isset($flags)&&(int) $flags->fetch_array()!=0){
-						$flags->data_seek(0);
-						while($flag=$flags->fetch_array()){
-							if($flag['type']=="creation"){
-								$creationname=$mysqli->query("SELECT name FROM creations WHERE id=".$flag['parentid'])->fetch_array();
-								$creationname[0]=$creationname[0]==""?"<span style='color:#E00'>Deleted creation</span>":'<a class="td" href="../creation/'.$flag['parentid'].'">'.$creationname[0].'</a>';
+					// Exactly like in admin_template except it doesn't cut off the names ever
+					if ( isset( $flags ) && ( (int) $flags->fetch_array() ) != 0) {
+						$flags->data_seek( 0 );
+						while ( $flag = $flags->fetch_array() ) {
+							switch ( $flag['type'] ) {
+							
+								case "creation":
+									$item_name = $mysqli->query( "SELECT name FROM creations WHERE id=".$flag['parentid'] )->fetch_array();
+									
+									if ( $item_name[0] == "") {
+										$item_name[0] = '<span style="color:#E00">Deleted creation</span>';
+									}
+									else {
+										$item_name[0] = '<a class="td" href="creation/' . $flag['parentid'] . '">' . trim( substr( strip_bbcode( $item_name[0] ), 0, $display_chars ) ) . '</a>';
+									}
+								break;
+								
+								case "comment":
+									$item_name = $mysqli->query( "SELECT comment FROM comments WHERE id=" . $flag['parentid'] )->fetch_array();
+									
+									if ( strlen( $item_name[0] ) > $display_chars ) {
+										$addendum = "&hellip;";
+									}
+									
+									if ( $item_name[0] == "") {
+										$item_name[0] = '<span style="color:#E00">Deleted comment</span>';
+									}
+									else {
+										$item_name[0] = '<a class="td" href="creation/' . get_creation_from_comment( $flag['parentid'], $mysqli ) . '#' . $flag['parentid'] . '">' . trim( substr( strip_bbcode( $item_name[0] ), 0, $display_chars ) ) . '</a>';
+									}
+								break;
+								
+								case "message":
+									$item_name = $mysqli->query( "SELECT message FROM messages WHERE id=" . $flag['parentid'] )->fetch_array();
+									
+									if ( strlen( $item_name[0] ) > $display_chars ) {
+										$addendum = "&hellip;";
+									}
+									
+									if ( $item_name[0] == "") {
+										$item_name[0] = '<span style="color:#E00">Deleted message</span>';
+									}
+									else {
+										$item_name[0] = '<a class="td" href="messages/' . get_sender_from_message( $flag['parentid'], $mysqli ) . '#' . $flag['parentid'] . '">' . trim( substr( strip_bbcode( $item_name[0]), 0, $display_chars ) ) . '</a>';
+									}
+								break;
 							}
-							else if ($flag['type']=="comment"){
-								$creationname=$mysqli->query("SELECT comment FROM comments WHERE id=".$flag['parentid'])->fetch_array();
-								$creationname[0]=$creationname[0]==""?"<span style='color:#E00'>Deleted comment</span>":'<a class="td" href="../creation/'.get_creation_from_comment($flag['parentid'],$mysqli).'#'.$flag['parentid'].'">'.strip_bbcode($creationname[0]).'</a>';
-							}
-							else if ($flag['type']=="message"){
-								$creationname=$mysqli->query("SELECT message FROM messages WHERE id=".$flag['parentid'])->fetch_array();
-								$creationname[0]=$creationname[0]==""?"<span style='color:#E00'>Deleted message</span>":'<a class="td" href="../messages/'.get_sender_from_message($flag['parentid'],$mysqli).'#'.$flag['parentid'].'">'.strip_bbcode($creationname[0]).'</a>';
-							}
-							echo '<tr id="'.$flag['id'].'">
-							<td class="'.$flag['type'].'" style="width:200px;">'.$creationname[0].'</td>
-							<td class="'.$flag['type'].'" style="width:80px;"><a class="td" href="user.php?id='.$flag['userid'].'">'.get_username_from_id($flag['userid'],$mysqli).'</a></td>
-							<td class="'.$flag['type'].'" style="width:80px;">'.date("m/d/Y",strtotime($flag['timestamp'])).'</td>
-							<td class="'.$flag['type'].'" style="width:400px;">'.$flag['content'].'<a class="deletebutton" href="?mode=flags&action=delete&id='.$flag['id'].'"></a></td>
+							echo '<tr id="' . $flag['id']. '">
+								<td class="' . $flag['type'] . '" style="width:200px;">' . $item_name[0] . '</td>
+								<td class="' . $flag['type'] . '" style="width:80px;"><a class="td" href="user/' . get_username_from_id( $flag['userid'], $mysqli ) . '">' . get_username_from_id( $flag['userid'], $mysqli ) . '</a></td>
+								<td class="' . $flag['type'] . '" style="width:80px;">' . date( "m/d/Y", strtotime( $flag['timestamp'] ) ) . '</td>
+								<td class="' . $flag['type'] . '" style="width:400px;">' . $flag['content'] . '<a class="deletebutton" href="admin/flags/' . $flag['id'] . '/delete"></a></td>
 							</tr>';
 						}
 					}
