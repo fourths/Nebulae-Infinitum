c340b14ca3400c83ae1af96399c1a6c5810a79bd
theflowersshesaidshesent@gmail.com <theflowersshesaidshesent@gmail.com@b7c36bab-8956-b2a8-0f3b-98a6af9334da>
2015-02-09 04:01:13 +0000

    fixed issue 62

--- trunk/errors/creation_hidden.php
+++ trunk/errors/creation_hidden.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
-<? require_once("config/config.php"); ?>
 <html>
-<head>
-<title><? echo $creation['name'] ?> | <? echo SITE_NAME ?></title>
-<link rel="stylesheet" type="text/css" href="templates/style.php" media="screen" />
-
-</head>
-
-<body>
-<? require_once(BASE_DIRECTORY."/templates/header.php") ?>
-<div class="container">
-<h1>Project hidden</h1>
-<img src="errors/hidden.png"/><br/>
-<? echo $creation['name'] ?> has been hidden by its owner. Sorry about that.<br/>
-<br/>
-<a href=".">Back to home</a>
-</div>
-</body>
+	<head>
+		<title>
+			Creation hidden | <?php echo SITE_NAME ?>
+		
+		</title>
+		<link rel="stylesheet" type="text/css" href="templates/style.php" media="screen" />
+	</head>
+	<body>
+		<?php require_once( BASE_DIRECTORY . "/templates/header.php" ) ?>
+		<div class="container">
+			<h1>Creation hidden</h1>
+			<div class="errorimage">
+				<img src="errors/hidden.png"/>
+			</div>
+			<div class="errordescription">
+				<? echo $creation['name'] ?> has been hidden by its owner. Sorry about that.<br/>
+				<br/>
+				<a href=".">Back to home</a>
+			</div>
+		</div>
+	</body>
 </html>
